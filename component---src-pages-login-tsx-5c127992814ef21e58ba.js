f1eab9bf7edd08af39566d77d90a470060470bd9
cheehau <puahcheehau@gmail.com>
2022-09-23 12:03:00 +0000

    Update main to output generated at d4018b3

---
+++ component---src-pages-login-tsx-5c127992814ef21e58ba.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinweb=self.webpackChunkinweb||[]).push([[47],{843:function(e,n,t){t.r(n),t.d(n,{Head:function(){return o},default:function(){return i}});var u=t(7294),r=t(982),c=t(20),l=function(){var e=(0,c.D3)().loginWithRedirect;return u.createElement("button",{onClick:function(){return e()}},"Log In")},i=function(){return u.createElement(r.Z,null,u.createElement(l,null))},o=function(){return u.createElement("title",null,"Home Page")}}}]);
+//# sourceMappingURL=component---src-pages-login-tsx-5c127992814ef21e58ba.js.map
